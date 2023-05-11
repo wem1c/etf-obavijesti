76d5a364cedbdf344764b0099e2fb0d2467a5c5c
Conor C. Peterson <conorpetersondev@gmail.com>
2023-05-11 12:23:20 +0200

    feat: add language definition

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    // https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+    // cnr == Montenegrin
+    locales: ["cnr"],
+    defaultLocale: "cnr",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
